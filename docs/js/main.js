9b62f78d5b273fbab4f1ee3f9928ff09df06337f
Anastasia <kostukewich5610@gmail.com>
2020-12-23 12:14:44 +0100

    contacts js

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,5 +7,28 @@ $(document).ready(function(){
       block: ""
     }
   })
+});
 
+/* form placeholder */
+const formItems = document.querySelector(".email");
+
+for (let item of formItems){
+  const thisParent = item.closest(".form-email");
+  const thisPlaceholder = thisParent.querySelector(".fake-placeholder");
+
+  /* если инпут в фокусе */
+  item.addEventListener("focus", function(){
+    thisPlaceholder.classList.add("active");
   });
+
+  /* если инпут теряет фокус */
+  item.addEventListener("blur", function(){
+
+    if(item.value.length > 0){
+      thisPlaceholder.classList.add("active");
+    }
+    else{
+      thisPlaceholder.classList.remove("active");
+    }
+  })
+}
