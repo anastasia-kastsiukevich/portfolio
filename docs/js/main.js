1406605d5032c3f2e75fc2c09c0d9923f91677a4
Anastasia <kostukewich5610@gmail.com>
2021-01-15 10:17:43 +0100

    last check

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   
-  let containerEL = document.querySelector("#project-card");
+  let containerEL = document.querySelector('#project-card');
 
   let mixer = mixitup(containerEL, {
     classNames: {
@@ -9,16 +9,84 @@ $(document).ready(function(){
   })
 });
 
-const toggleMenu = document.querySelector(".toggle-menu");
-const mobMenu = document.querySelector(".mobile-menu");
+const toggleMenu = document.querySelector('.toggle-menu');
+const mobMenu = document.querySelector('.mobile-menu');
 const overlay = document.querySelector("#overlay");
 const bodyEL = document.body;
 
-toggleMenu.addEventListener("click", function(){
+//клик по гамбургеру
+toggleMenu.addEventListener('click', function(){
   
-  this.classList.toggle("active");
-  mobMenu.classList.toggle("active");
-  overlay.classList.toggle("active");
-  bodyEl.classList.toggle("noscroll");
-})
- 
+  this.classList.toggle('active');
+  mobMenu.classList.toggle('active');
+  overlay.classList.toggle('active');
+  bodyEl.classList.toggle('noscroll');
+});
+
+//клик по мобильному меню
+mobMenu.addEventListener('click', function(){
+  this.classList.remove('active');
+  toggleMenu.classList.remove('active');
+  overlay.classList.remove('active');
+  bodyEl.classList.remove('noscroll');
+});
+
+//клик по overlay
+overlay.addEventListener('click', function(){
+  this.classList.remove('active');
+  toggleMenu.classList.remove('active');
+  mobMenu.classList.remove('active');
+  bodyEl.classList.remove('noscroll');
+});
+
+/* //form placeholder
+const formItems = document.querySelectorAll('.form-field');
+
+for(let item of formItems){
+  const thisParent = item.closest('.form-item');
+  const thisPlaceholder = thisParent.querySelector('.fake-placeholder');
+  //Если инпут в фокусе
+  item.addEventListener('focus', function(){
+    thisPlaceholder.classList.add('active');
+  });
+
+  //Если инпут теряет фокус
+  item.addEventListener('blur', function(){
+    if(item.value.lenght > 0){
+      thisPlaceholder.classList.add('active');
+    }
+
+    else{
+      thisPlaceholder.classList.remove('active');
+    }
+  })
+};
+
+// Form validate
+$('.contacts-form').validate({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    },
+    subject: {
+      required: true
+    },
+    massage: {
+      required: true
+    }
+  },
+
+  messages: {
+    email: {
+      required: 'Введите email',
+      email: 'отсутствует символ @'
+    },
+    subject: {
+      required: 'Введите тему сообщения'
+    },
+    message: {
+      required: 'Введите текст сообщения'
+    }
+  }
+}) */
