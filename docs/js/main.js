ae6e72afdd5d97dbc034165847d6858f1e24849a
Anastasia <kostukewich5610@gmail.com>
2021-01-20 13:30:53 +0100

    nice things

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,42 +7,109 @@ $(document).ready(function(){
       block: ""
     }
   })
-});
 
-const toggleMenu = document.querySelector('.toggle-menu');
-const mobMenu = document.querySelector('.mobile-menu');
-const overlay = document.querySelector("#overlay");
-const bodyEL = document.body;
+  // Подключение точек пагинации справа page-nav
+	$('#page-nav').onePageNav({
+		currentClass: 'active',
+		changeHash: false,
+		scrollSpeed: 750,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'swing',
+		begin: function () {},
+		end: function () {},
+		scrollChange: function ($currentListItem) {}
+	});
 
-//клик по гамбургеру
-toggleMenu.addEventListener('click', function(){
-  
+    // Form validate
+  $('.contact-form').validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      subject: {
+        required: true
+      },
+      message: {
+        required: true
+      }
+    },
+
+    messages: {
+      email: {
+        required: 'Введите email',
+        email: 'отсутствует символ @'
+      },
+      subject: {
+        required: 'Введите тему сообщения'
+      },
+      message: {
+        required: 'Введите текст сообщения'
+      }
+    },
+
+    submitHandler: function (form) {
+      ajaxFormSubmit();
+    }
+  })
+
+  // Функция AJAX запроса на сервер
+
+  function ajaxFormSubmit() {
+    let string = $('.contact-form').serialize(); //Сохраняем данные введенные в форму в строку
+
+    // Формируем AJAX запрос
+    $.ajax({
+      type: "POST", //Тип запроса - POST
+      url: "php/mail.php", //Куда отправляем запрос
+      data: string, // Какие данные отправляем, в данном случае отправляем переменную string
+
+      // Функция если все прошло успешно
+      success: function (html) {
+        $(".contact-form").slideUp(800);
+        $('#answer').html(html);
+      }
+    })
+    // Чтобы по Submit больше ничего не выполнялось - делаем возврат false, чтобы прервать цепочку срабатывания остальных функций
+    return false;
+  }
+
+
+  const toggleMenu = document.querySelector('.toggle-menu');
+  const mobMenu = document.querySelector('.mobile-menu');
+  const overlay = document.querySelector("#overlay");
+  const bodyEL = document.body;
+
+  //клик по гамбургеру
+  toggleMenu.addEventListener('click', function(){
+
   this.classList.toggle('active');
   mobMenu.classList.toggle('active');
   overlay.classList.toggle('active');
   bodyEl.classList.toggle('noscroll');
-});
+  });
 
-//клик по мобильному меню
-mobMenu.addEventListener('click', function(){
+  //клик по мобильному меню
+  mobMenu.addEventListener('click', function(){
   this.classList.remove('active');
   toggleMenu.classList.remove('active');
   overlay.classList.remove('active');
   bodyEl.classList.remove('noscroll');
-});
+  });
 
-//клик по overlay
-overlay.addEventListener('click', function(){
+  //клик по overlay
+  overlay.addEventListener('click', function(){
   this.classList.remove('active');
   toggleMenu.classList.remove('active');
   mobMenu.classList.remove('active');
   bodyEl.classList.remove('noscroll');
-});
+  });
 
-/* //form placeholder
-const formItems = document.querySelectorAll('.form-field');
+  //form placeholder
+  const formItems = document.querySelectorAll('.form-field');
 
-for(let item of formItems){
+  for(let item of formItems){
   const thisParent = item.closest('.form-item');
   const thisPlaceholder = thisParent.querySelector('.fake-placeholder');
   //Если инпут в фокусе
@@ -52,7 +119,7 @@ for(let item of formItems){
 
   //Если инпут теряет фокус
   item.addEventListener('blur', function(){
-    if(item.value.lenght > 0){
+    if(item.value.length > 0){
       thisPlaceholder.classList.add('active');
     }
 
@@ -60,33 +127,10 @@ for(let item of formItems){
       thisPlaceholder.classList.remove('active');
     }
   })
-};
+  };
+  
+})
 
-// Form validate
-$('.contacts-form').validate({
-  rules: {
-    email: {
-      required: true,
-      email: true
-    },
-    subject: {
-      required: true
-    },
-    massage: {
-      required: true
-    }
-  },
+    
 
-  messages: {
-    email: {
-      required: 'Введите email',
-      email: 'отсутствует символ @'
-    },
-    subject: {
-      required: 'Введите тему сообщения'
-    },
-    message: {
-      required: 'Введите текст сообщения'
-    }
-  }
-}) */
+
